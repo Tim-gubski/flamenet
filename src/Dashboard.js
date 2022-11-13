94c24cfca5c19f9ef6faa56e3e44c9bc1fdf6c83
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2022-11-13 03:00:57 -0500

    Dashboard changed

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,54 @@ import okanaganNodeMap from "./nodemaps/okanagan-nodemap.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/fontawesome-free-brands";
 import ImageMapper from "react-img-mapper";
+import { Row, Col, Button, Container, Table } from "react-bootstrap";
+
+const green = "#3dfc03";
+
+const defaultNodes = [
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [205, 80, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [205, 120, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Browne Lake",
+    shape: "circle",
+    coords: [369, 220, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [190, 250, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [190, 250, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [230, 390, 8],
+    preFillColor: green,
+  },
+  {
+    name: "Okanagan 1",
+    shape: "circle",
+    coords: [430, 290, 8],
+    preFillColor: green,
+  },
+];
 
 const Dashboard = () => {
   const db = getDatabase();
@@ -15,7 +63,7 @@ const Dashboard = () => {
   useEffect(() => {
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
-      setNodeData(data);
+      setNodeData({ ...data });
       console.log(data);
     });
   }, []);
@@ -24,67 +72,98 @@ const Dashboard = () => {
     // GET JSON FROM BELOW URL AS AN EXAMPLE
     areas: [
       {
+        name: "node1",
         shape: "circle",
         coords: [105, 80, 8],
         preFillColor:
-          nodeData["node1"]?.status == "on fire" ? "#d84c2c" : "green",
+          nodeData["node1"]?.status == "on fire" ? "#fc0303" : green,
       },
       {
+        name: "node2",
         shape: "circle",
         coords: [185, 50, 8],
         preFillColor:
-          nodeData["node2"]?.status == "on fire" ? "#d84c2c" : "green",
+          nodeData["node2"]?.status == "on fire" ? "#fc0303" : green,
       },
       {
+        name: "node3",
         shape: "circle",
         coords: [273, 25, 8],
         preFillColor:
-          nodeData["node3"]?.status == "on fire" ? "#d84c2c" : "green",
+          nodeData["node3"]?.status == "on fire" ? "#fc0303" : green,
       },
+      ...defaultNodes,
     ],
   };
 
   return (
-    <div className="App">
-      <img className="logo" src={flamenetLogo} alt="FlameNet" />
-      <h1>Okanagan Nodemap:</h1>
-      <ImageMapper src={okanaganNodeMap} map={MAP} />
-      <table id="rcorners1">
-        <thead>
-          <tr>
-            <th>Node Name</th>
-            <th>Location</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(nodeData).map((key) => {
-            return (
-              <tr
-                className={
-                  nodeData[key]?.status == "on fire"
-                    ? "fire"
-                    : nodeData[key]?.status == "not on fire"
-                    ? "no-fire"
-                    : "pass"
-                }
-              >
-                <td>{key}</td>
-                <td>{nodeData[key]?.location}</td>
-                <td>{nodeData[key]?.status}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div className="links">
-        <div data-id="github">
-          <a href="https://github.com/Tim-gubski/flamenet">
-            <FontAwesomeIcon className="icon" icon={faGithub} size="5x" />
-          </a>
+    <Container>
+      <center>
+        <br />
+        <img className="logo" src={flamenetLogo} alt="FlameNet" />
+        <br />
+        <br />
+        <h1>Okanagan Nodemap:</h1>
+        <br />
+        <br />
+        <Row>
+          <Col xs={6}>
+            <ImageMapper src={okanaganNodeMap} map={MAP} width={500} />
+          </Col>
+          <Col xs={6} className="align-items-center">
+            <Table id="rcorners1" responsive>
+              <thead>
+                <tr>
+                  <th>Node Name</th>
+                  <th>Location</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(nodeData).map((key) => {
+                  return (
+                    <tr
+                      className={
+                        nodeData[key]?.status == "on fire"
+                          ? "fire"
+                          : nodeData[key]?.status == "not on fire"
+                          ? "no-fire"
+                          : "pass"
+                      }
+                    >
+                      <td>{key}</td>
+                      <td>{nodeData[key]?.location}</td>
+                      <td>{nodeData[key]?.status}</td>
+                    </tr>
+                  );
+                })}
+                {defaultNodes.map((i, index) => {
+                  return (
+                    <tr className="no-fire">
+                      <td>node{index + 3}</td>
+                      <td>{i.coords.slice(0, 2).join(", ")}</td>
+                      <td>Not On Fire</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+
+        <br />
+        <br />
+        <div className="links">
+          <div data-id="github">
+            <a href="https://github.com/Tim-gubski/flamenet">
+              <FontAwesomeIcon className="icon" icon={faGithub} size="5x" />
+            </a>
+          </div>
         </div>
-      </div>
-    </div>
+      </center>
+      <br />
+      <br />
+    </Container>
   );
 };
 
