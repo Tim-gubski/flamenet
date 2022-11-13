1bd74ceb8220259653d8d3617d7da3cef4874515
Daniel Sabourov <dan.sabo@live.ca>
2022-11-13 01:01:34 -0700

    home page changes

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,40 @@
+import flamenetLogo from "./flamenet-logo.png";
+import detector from "./detector.png";
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/fontawesome-free-brands";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  useEffect(() => {
+    document.title = 'FlameNet Homepage';
+  }, []);
+
+  let navigate = useNavigate(); 
+  const dashboard = () =>{ 
+    let path = `/dashboard`; 
+    navigate(path);
+  }
+
+  const Columns = () => 
+  <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 300}}>
+    <div>
+      <h1>Detect</h1>
+      <img src={detector}/>
+      </div>
+
+    <div><h1>Dispatch</h1></div>
+    <div><h1>Defend</h1></div>
+  </div>
+
+  return (
+  <div className="App">
+    <img className="logo" src={flamenetLogo} alt="FlameNet" />
+    <Columns />
+    <button class="button-78" onClick={dashboard}>View Example Dashboard</button>
+    <a class="button-78" href="https://github.com/Tim-gubski/flamenet">View on Github</a>
+  </div>
+  );
 };
 
 export default Home;
