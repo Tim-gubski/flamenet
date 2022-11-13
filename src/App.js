3e17ab2a0fe16ede8e4135f4f457eb0c2a5e7e2e
Daniel Sabourov <dan.sabo@live.ca>
2022-11-12 21:48:56 -0700

    Frontend Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { useEffect, useState } from "react";
+import flamenetLogo from "./flamenet-logo.png";
+import okanaganNodeMap from "./nodemaps/okanagan-nodemap.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/fontawesome-free-brands';
+import ImageMapper from 'react-img-mapper';
 
 const firebaseConfig = {
   apiKey: "***REMOVED***",
@@ -29,12 +33,22 @@ function App() {
       setNodeData(data);
       console.log(data);
     });
-  }, []);
+  }, [])
+    const MAP = {
+      name: 'my-map',
+      // GET JSON FROM BELOW URL AS AN EXAMPLE
+      areas: [
+        {"shape": "circle", "coords": [105, 80, 8], "preFillColor": nodeData["node1"]?.status == "on fire" ? "#d84c2c" : "green"},
+        {"shape": "circle", "coords": [185, 50, 8], "preFillColor": nodeData["node2"]?.status == "on fire" ? "#d84c2c" : "green"},
+        {"shape": "circle", "coords": [273, 25, 8], "preFillColor": nodeData["node3"]?.status == "on fire" ? "#d84c2c" : "green"}],
+    };
 
   return (
     <div className="App">
-      <h1>FlameNet</h1>
-      <table>
+      <img className="logo" src={flamenetLogo} alt="FlameNet"/>
+      <h1>Okanagan Nodemap:</h1>
+      <ImageMapper src={okanaganNodeMap} map={MAP}/>
+      <table id="rcorners1">
         <thead>
           <tr>
             <th>Node Name</th>
@@ -45,7 +59,7 @@ function App() {
         <tbody>
           {Object.keys(nodeData).map((key) => {
             return (
-              <tr>
+              <tr className={nodeData[key]?.status == "on fire" ? "fire" : nodeData[key]?.status == "not on fire" ? "no-fire" : "pass"}>
                 <td>{key}</td>
                 <td>{nodeData[key]?.location}</td>
                 <td>{nodeData[key]?.status}</td>
@@ -54,7 +68,13 @@ function App() {
           })}
         </tbody>
       </table>
-      <header className="App-header">{JSON.stringify(nodeData)}</header>
+      <div className="links">
+        <div data-id="github">
+          <a href="https://github.com/Tim-gubski/flamenet">
+            <FontAwesomeIcon className="icon" icon={faGithub} size="5x"/>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
